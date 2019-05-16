bf6914ba6841503e6ca9870e953238002217a2d7
gaonl <gnlyjy@163.com>
2019-05-16 23:02:55 +0800

    add user dao tests

---
+++ src/test/java/com/example/demo/dao/UserDaoTests.java
@@ -0,0 +1,24 @@
+package com.example.demo.dao;
+
+import com.example.demo.domain.User;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class UserDaoTests {
+
+	@Autowired
+	private UserDao userDao;
+
+	@Test
+	public void getById() {
+		User user = userDao.getById(1);
+		Assert.assertNotNull(user);
+	}
+
+}
