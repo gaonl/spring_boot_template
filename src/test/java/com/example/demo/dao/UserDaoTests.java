864c26942fddff08f4948deedacef08e09e9b201
gaonl <gnlyjy@163.com>
2019-05-17 00:02:11 +0800

    fix bug

--- src/test/java/com/example/demo/dao/UserDaoTests.java
+++ src/test/java/com/example/demo/dao/UserDaoTests.java
@@ -19,7 +19,7 @@ public class UserDaoTests {
 	public void getById() {
 		User user = userDao.getById(1);
 		Assert.assertNotNull(user);
-		Assert.assertEquals("username not same!!",user.getName(),"gao");
+		Assert.assertEquals("username not same!!",user.getName(),"gaonl");
 	}
 
 }
