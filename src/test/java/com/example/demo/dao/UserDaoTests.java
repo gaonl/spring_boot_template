59d71d3dca385a161c42ead3db8822a716c91dd3
gaonl <gnlyjy@163.com>
2019-05-19 10:44:14 +0800

    add test

--- src/test/java/com/example/demo/dao/UserDaoTests.java
+++ src/test/java/com/example/demo/dao/UserDaoTests.java
@@ -8,6 +8,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.List;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class UserDaoTests {
@@ -20,6 +22,16 @@ public class UserDaoTests {
 		User user = userDao.getById(1);
 		Assert.assertNotNull(user);
 		Assert.assertEquals("username not same!!",user.getName(),"gaonl");
+	}
+
+	@Test
+	public void getByName() {
+		List<User> users = userDao.getByName("gaonl");
+		Assert.assertNotNull(users);
+		Assert.assertTrue(users.size() >= 1);
+		for(User user: users){
+			Assert.assertEquals("name not equals!!","gaonl",user.getName());
+		}
 	}
 
 }
